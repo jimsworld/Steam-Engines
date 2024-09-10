24dcb860253735a629fa41007bc44e8582b64622
jimsworld <jamesn_1991@hotmail.co.uk>
2024-09-10 14:08:06 +0100

    Added loop to the start_menu function - Introduced a loop to serve as a mechanic that allows the user to confirm their choice before proceeding

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ loco_02 = Loco_Steam("Dolgoch", 0, 0, 0, 0, 8, 10, 10, 12)
 loco_list = [loco_01, loco_02]
 
 # Start menu to select a locomotive
-def start_menu(loco_list):
+def loco_menu(loco_list):
     
     print("\n--- Start Menu"
           "\nSteam Locomotive List:")
@@ -34,6 +34,41 @@ def start_menu(loco_list):
     return chosen_loco
 
 
+# Prepare the locomotive with coal and water
+def prepare_loco(chosen_loco):
+    add_coal = input("\nAdd some Coal?"
+                        "\n'1': Full Coal"
+                        "\n'2': Choose amount"
+                        "\n'3': Skip\n"
+                        "\n> ")
+    match add_coal:
+        case "1":
+            chosen_loco.current_coal = chosen_loco.max_coal
+            print("Full Coal added.")
+        case "2":
+            coal_amount = int(input("Enter the amount of coal to add: "))
+            chosen_loco.current_coal += coal_amount
+        case "3":
+            print("No Coal added.")
+    print(f"{chosen_loco.current_coal} / {chosen_loco.max_coal} Coal added")
+    
+    add_water = input("\nAdd some Water?"
+                        "\n'1': Full Water"
+                        "\n'2': Choose amount"
+                        "\n'3': Skip\n"
+                        "\n> ")
+    match add_water:
+        case "1":
+            chosen_loco.current_water = chosen_loco.max_water
+            print("Full Water added.")
+        case "2":
+            water_amount = int(input("Enter the amount of Water to add: "))
+            chosen_loco.current_water += water_amount
+        case "3":
+            print("No Water added.")
+    print(f"{chosen_loco.current_water} / {chosen_loco.max_water} Water added")
+
+
 # Stat menu to display the locomotive stats
 def stat_menu(chosen_loco):
     print(f"\n--- {chosen_loco.name} Stats ---"
@@ -43,67 +78,8 @@ def stat_menu(chosen_loco):
             f"\nMax Speed: {chosen_loco.max_speed}")
 
 
-# Start the game
-def start_game(chosen_loco):
-    print("\nLet's get ready..."
-            "\nWhat would you like to do?\n"
-            "\n'1': Prepare your locomotive"
-            "\n'2': View your locomotive stats"
-            "\n'3': View Station Map"
-            "\n'help': Show commands")
-
-    start_game_choice = input("\n> ")
-    match start_game_choice:
-
-        case "1":
-            add_coal = input("\nAdd some Coal?"
-                                "\n'1': Full Coal"
-                                "\n'2': Choose amount"
-                                "\n'3': Skip\n"
-                                "\n> ")
-            match add_coal:
-                case "1":
-                    chosen_loco.current_coal = chosen_loco.max_coal
-                    print("Full Coal added.")
-                case "2":
-                    coal_amount = int(input("Enter the amount of coal to add: "))
-                    chosen_loco.current_coal += coal_amount
-                case "3":
-                    print("No Coal added.")
-            
-            print(f"{chosen_loco.current_coal} / {chosen_loco.max_coal} Coal added")
-            
-            add_water = input("\nAdd some Water?"
-                                "\n'1': Full Water"
-                                "\n'2': Choose amount"
-                                "\n'3': Skip\n"
-                                "\n> ")
-            match add_water:
-                case "1":
-                    chosen_loco.current_water = chosen_loco.max_water
-                    print("Full Water added.")
-                case "2":
-                    water_amount = int(input("Enter the amount of Water to add: "))
-                    chosen_loco.current_water += water_amount
-                case "3":
-                    print("No Water added.")
-            
-            print(f"{chosen_loco.current_water} / {chosen_loco.max_water} Water added")
-    
-        case "2":
-            stat_menu(chosen_loco)
-        
-        case "3":
-            print("Feature coming soon.")
-
-        case "help":
-            show_menu()
-    
-    start_game_choice = input("\nAnything else?: ")
-
-
-# Input menu shows commands
-def show_menu():
+# Command list
+def command_list():
     print("\nEnter command:"
           "\n'help':       shows this menu"
           "\n'start':      start engine"
@@ -114,6 +90,38 @@ def show_menu():
           "\n'4':          accelerate"
           "\n'5':          brake"
           "\n'quit':       exits.")
+
+
+# Start the game
+def start_game(chosen_loco):
+    print("\nLet's get ready..."
+            "\nWhat would you like to do?\n"
+            "\n'1': Prepare your locomotive"
+            "\n'2': View your locomotive stats"
+            "\n'3': View Station Map"
+            "\n'help': Show commands"
+            "\n'0': Confirm and Start")
+
+    def start_menu(chosen_loco):
+        while True:
+            start_menu_choice = input("\n> ")
+            match start_menu_choice:
+                case "1":
+                    prepare_loco(chosen_loco)
+                    print("\nPreperations complete. What would you like to do?")
+                case "2":
+                    stat_menu(chosen_loco)
+                case "3":
+                    print("Station Map coming soon.")
+                case "help":
+                    command_list()
+                case "0":
+                    break
+                case _:
+                    print("Invalid command. Please try again.")
+
+    
+    start_menu(chosen_loco)
 
 
 #  Test the steam locomotive
@@ -127,9 +135,7 @@ def test_loco(chosen_loco):
 
 # Main loop
 def main():
-    chosen_loco = start_menu(loco_list)
-
-    stat_menu(chosen_loco)
+    chosen_loco = loco_menu(loco_list)
 
     start_game(chosen_loco)
 
